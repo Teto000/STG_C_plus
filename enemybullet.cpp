643e8b70a104ba7bb905dad6f2eaa3cf901b31fd
student <student@S-JOBI19-046>
2022-08-22 09:17:48 +0900

    敵の弾追加

---
+++ enemybullet.cpp
@@ -0,0 +1,130 @@
+//===================================
+//
+// 弾の処理
+// Author : Sato Teruto
+//
+//===================================
+
+//------------------------
+// インクルード
+//------------------------
+#include "enemybullet.h"
+#include "main.h"
+#include "renderer.h"
+#include "object2D.h"
+#include "application.h"
+#include "damage.h"
+#include "enemy.h"
+
+//===========================
+// コンストラクタ
+//===========================
+CEnemyBullet::CEnemyBullet() : CObject2D()
+{
+	m_pos = D3DXVECTOR3(0.0f, 0.0f, 0.0f);		//位置
+	m_move = D3DXVECTOR3(0.0f, 0.0f, 0.0f);		//移動量
+	m_rot = D3DXVECTOR3(0.0f, 0.0f, 0.0f);		//回転
+	m_nLife = 0;				//寿命
+	m_fWidth = 0.0f;			//幅
+	m_fHeight = 0.0f;			//高さ
+}
+
+//===========================
+// デストラクタ
+//===========================
+CEnemyBullet::~CEnemyBullet()
+{
+
+}
+
+//===========================
+// 初期化
+//===========================
+HRESULT CEnemyBullet::Init(D3DXVECTOR3 pos)
+{
+	//----------------------------
+	// メンバ変数の初期化
+	//----------------------------
+	m_pos = pos;
+	m_nLife = 10;
+	m_fWidth = 10;
+	m_fHeight = 10;
+
+	CObject2D::Init(m_pos);
+
+	CObject2D::SetSize(m_fWidth, m_fHeight);			//サイズの設定
+	CObject2D::SetTexture(CTexture::TEXTURE_BULLET);	//テクスチャの設定
+
+	return S_OK;
+}
+
+//===========================
+// 終了
+//===========================
+void CEnemyBullet::Uninit()
+{
+	CObject2D::Uninit();
+}
+
+//===========================
+// 更新
+//===========================
+void CEnemyBullet::Update()
+{
+	CObject2D::Update();
+
+	m_rot.x = 10.0f;
+
+	SetVtxCIE_Rot(m_pos, m_rot, m_fWidth, m_fHeight);
+
+	//移動量の加算
+	m_pos = CObject2D::AddMove(m_move);
+
+	//寿命の減少
+	m_nLife--;
+
+	//寿命が尽きた
+	if (m_nLife <= 0.0f)
+	{
+		Uninit();
+		return;
+	}
+	//画面端の処理
+	else if (m_pos.x >= SCREEN_WIDTH)
+	{
+		Uninit();
+		return;
+	}
+}
+
+//===========================
+// 描画
+//===========================
+void CEnemyBullet::Draw()
+{
+	CObject2D::Draw();
+}
+
+//===========================
+// 生成
+//===========================
+CEnemyBullet *CEnemyBullet::Create(D3DXVECTOR3 pos, D3DXVECTOR3 move)
+{
+	CEnemyBullet *pBullet = nullptr;
+
+	//----------------------------------
+	// バレットの生成と初期化
+	//----------------------------------
+	pBullet = new CEnemyBullet;	//生成
+
+	if (pBullet != nullptr)
+	{//NULLチェック
+	 //初期化
+		pBullet->m_move = move;		//移動量の代入
+		pBullet->Init(pos);
+
+		pBullet->SetObjType(OBJTYPE_BULLET);
+	}
+
+	return pBullet;
+}
---
+++ enemybullet.h
@@ -0,0 +1,49 @@
+//=====================================
+//
+// 敵の弾ヘッダー
+// Author : Sato Teruto
+//
+//=====================================
+#ifndef _ENEMYBULLET_H_
+#define _ENEMYBULLET_H_	
+
+//--------------------------
+// インクルード
+//--------------------------
+#include "object2D.h"
+
+//--------------------------
+// バレットクラス
+//--------------------------
+class CEnemyBullet : public CObject2D
+{
+public:
+	CEnemyBullet();				//コンストラクタ
+	~CEnemyBullet() override;	//デストラクタ
+
+	//----------------
+	// メンバ関数
+	//----------------
+	HRESULT Init(D3DXVECTOR3 pos) override;
+	void Uninit() override;
+	void Update() override;
+	void Draw()	  override;
+
+	//----------------
+	// 静的メンバ関数
+	//----------------
+	static CEnemyBullet *Create(D3DXVECTOR3 pos, D3DXVECTOR3 move);
+
+private:
+	//----------------
+	// メンバ変数
+	//----------------s
+	D3DXVECTOR3 m_pos;		//位置
+	D3DXVECTOR3 m_move;		//移動量
+	D3DXVECTOR3 m_rot;		//回転
+	int m_nLife;			//寿命
+	float m_fWidth;			//幅
+	float m_fHeight;		//高さ
+};
+
+#endif // !_BULLET_H_
